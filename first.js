2645efb67b9961b7335e8b513ee4d76f31eaffdd
goodness <oluwadamilolaadebayo019@gmail.com>
2022-06-22 14:27:20 -0700

    changed from document.write to alert

--- first.js
+++ first.js
@@ -1,3 +1,3 @@
-document.write("My name is Adebayo Goodness Oluwadamilola \n");
-document.write("I am 5'8 tall \n");
-document.write("I am a Nigerian \n");
+alert("My name is Adebayo Goodness Oluwadamilola \n");
+alert("I am 5'8 tall \n");
+alert("I am a Nigerian \n");
