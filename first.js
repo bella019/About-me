9a179a9cc49e428b8044e4b632ec4ec1c277c660
goodness <oluwadamilolaadebayo019@gmail.com>
2022-06-22 17:36:15 -0700

    modified

--- first.js
+++
@@ -1,3 +0,0 @@
-alert("My name is Adebayo Goodness Oluwadamilola \n");
-alert("I am 5'8 tall \n");
-alert("I am a Nigerian \n");
---
+++ index.js
@@ -0,0 +1,4 @@
+alert("My name is Adebayo Goodness Oluwadamilola\n");
+alert("I am 5'8 tall\n");
+alert("I am a Nigerian\n");
+
